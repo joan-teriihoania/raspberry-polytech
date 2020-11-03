def03ee29c059753143f961901cd24d322b9230e
zahra-ahlal <71870219+zahra-ahlal@users.noreply.github.com>
2020-11-03 14:21:18 +0100

    Update textospeech.py

---
+++ textospeech.py
@@ -0,0 +1,9 @@
+import gtts
+
+def toText(str,lang) :
+  # make request to google to get synthesis
+  tts = gtts.gTTS(str,lang)
+  # save the audio file
+  tts.save("audio.mp3")
+  
+ #methode pour voir toutes les langues disponibles : gtts.lang.tts_langs()
