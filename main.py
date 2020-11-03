0b8cb33da1cd8163db866da9d8b34432ccc89057
zahra-ahlal <71870219+zahra-ahlal@users.noreply.github.com>
2020-11-03 14:26:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import translater
 import speechtotext
+import textospeech
 import core
 
 core.echo(" >> " + translater.translate("Salut tout le monde"))
 core.echo(" >> " + speechtotext.toText("Enregistrement.wav"))
+textospeech.toSpeech("Ciao","it")
