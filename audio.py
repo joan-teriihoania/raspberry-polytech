b1459738513a71aa7cf08f5cfea92abb6b458c2f
Joprocorp <joprocorp@gmail.com>
2020-11-24 21:59:16 +0100

    Add logger and prepare main loop

--- audio.py
+++ audio.py
@@ -14,11 +14,12 @@ def textToSpeech(string,lang, filepath="./ressources/audio.mp3"):
             tts = gtts.gTTS(text=string, lang=lang)
             tts.save(filepath)
             core.overecho('Converting text ('+lang+') into audio transcripts ...' + core.done)
-            return
+            return True
         except KeyboardInterrupt:
             return
         except ValueError:
             core.overecho('Converting text ('+lang+') into audio transcripts ...' + core.failed)
+            return False
         
  #methode pour voir toutes les langues disponibles : gtts.lang.tts_langs()
 
@@ -35,10 +36,17 @@ def speechToText(filepath, lang="fr-FR"):
         # recoginize_() method will throw a request error if the API is unreachable, hence using exception handling
         try:
             # using google speech recognition
-            core.echo('Converting audio transcripts ('+lang+') into text ...')
-            text = r.recognize_google(audio_text, language = "fr-FR")
-            core.overecho('Converting audio transcripts ('+lang+') into text ...' + core.done)
+            core.echo('Converting audio transcripts ('+lang+') into text ...', end="")
+            text = r.recognize_google(audio_text, language = lang)
+            print(core.done)
             return text
         
-        except:
-            core.overecho('Converting audio transcripts ('+lang+') into text ...' + core.failed)
+        except sr.UnknownValueError:
+            print(core.failed)
+            core.echo("Google Speech Recognition could not understand audio", "ERROR")
+            return False
+        except sr.RequestError as e:
+            print(core.failed)
+            core.echo("Could not request results from Google Speech Recognition service", "ERROR")
+            core.echo(e, "ERROR")
+            return False
