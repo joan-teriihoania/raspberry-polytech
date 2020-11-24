8d093613e803d32f0cbc20b17bf55b873bd76e1a
Joprocorp <joprocorp@gmail.com>
2020-11-25 00:45:49 +0100

    Installing main loop with emergency termination for fatal error

--- main.py
+++ main.py
@@ -12,52 +12,49 @@ import time
 
 def main():
     driverI2C.setColor('white')
+    driverI2C.display("Enregistrement")
+    if not(driverMicro.listen()):
+        core.echo("An error occured during the record", "ERROR")
+        return False
+    driverSpeaker.play("ressources/test1.wav")
+
+    driverI2C.display("Conversion")
+    text = audio.speechToText("ressources/test1.wav")
+    if(text == False):
+        core.echo("An error occured during conversion from audio to text", "ERROR")
+        return False
     
+
+    core.echo(" >> " + str(text))
+    driverI2C.display("Traduction")
+    trad = translater.translate(text)
+    if trad == None:
+        core.echo("An error occured during translation", "ERROR")
+        return False
+
+    driverI2C.display("Synthétisation")
+    core.echo(trad)
+    if not(audio.textToSpeech(trad, "en")):
+        core.echo("An error occured during conversion from text to audio", "ERROR")
+        return False
+    
+    driverSpeaker.play('ressources/audio.mp3')
+
+
+
+while True:
     try:
-        driverI2C.display("Enregistrement")
-        if not(driverMicro.listen()):
-            core.echo("An error occured during the record", "ERROR")
-            return False
-
-        driverI2C.display("Conversion")
-        text = audio.speechToText("ressources/test1.wav")
-        if(text == False):
-            core.echo("An error occured during conversion from audio to text", "ERROR")
-            return False
-        
-
-        core.echo(" >> " + str(text))
-        driverI2C.display("Traduction")
-        trad = translater.translate(text)
-        if trad == None:
-            core.echo("An error occured during translation", "ERROR")
-            return False
-
-        driverI2C.display("Synthétisation")
-        core.echo(trad)
-        if not(audio.textToSpeech(trad, "en")):
-            core.echo("An error occured during conversion from text to audio", "ERROR")
-            return False
-        
-        driverSpeaker.play('ressources/audio.mp3')
+        if(main() == False):
+            driverI2C.setRGB(255,0,0)
+            driverI2C.display('Je n\'ai pas compris')
+            audio.say("I am sorry, I did not understand.", "en")
     except KeyboardInterrupt:
         core.terminate(-1, "Interrupted before completion by user")
     except SystemExit:
-        return False
-        pass
+        continue
     except:
         exc_type, exc_value, exc_traceback = sys.exc_info()
         core.echo("Unexpected error: "+str(exc_type), "ERROR")
         stack_array = traceback.format_tb(exc_traceback)
         stack_array.append(exc_value)
-        core.echo(stack_array, "FATAL")
-        return False
-
-while True:
-    r = main()
-    if(r == False):
-        driverI2C.setColor('red')
-        driverI2C.display('Erreur fatale')
-        time.sleep(3)
-
-core.terminate(0)
+        core.terminate(-1, stack_array)
