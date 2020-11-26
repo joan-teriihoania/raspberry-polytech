e304c65f32baf6e18384cb368b51fc3d6176b8c5
Joprocorp <joprocorp@gmail.com>
2020-11-26 14:52:32 +0100

    Adding error handling for translation usage limits

--- translater.py
+++ translater.py
@@ -1,8 +1,15 @@
 from translate import Translator
+import html
+import core
 
 
-
-def translate(str, from_lang="french", to_lang="english"):
+def translate(text, from_lang="french", to_lang="english"):
     translator= Translator(from_lang=from_lang, to_lang=to_lang)
-    translation = translator.translate(str)
+    translation = translator.translate(text)
+    
+    if("HTTPS://MYMEMORY.TRANSLATED.NET/DOC/USAGELIMITS.PHP" in translation):
+        core.terminate(1)
+
+    if(isinstance(translation, str)):
+        translation = html.unescape(translation)
     return translation
