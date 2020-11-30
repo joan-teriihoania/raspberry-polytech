1907041cd1c10d19bf83fd3dadcc10f50485149e
Joprocorp <joprocorp@gmail.com>
2020-11-30 09:36:57 +0100

    Adding a watchdog script with check file verif

---
+++ watchdog.sh
@@ -0,0 +1,11 @@
+
+FILE=/home/jopro/raspberry-polytech/ressources/tmp_active_check
+
+if test -f "$FILE"; then
+    rm "$FILE"
+fi
+echo "CHECKING FOR ACTIVITY (THIS FILE SHOULD BE REMOVED BY 3 SECONDS AFTER ITS CREATION IF IN INSTANCE IS RUNNING)" > "$FILE" 
+sleep 3
+if test -f "$FILE"; then
+    sudo python3.7 /home/jopro/raspberry-polytech/main.py
+fi
