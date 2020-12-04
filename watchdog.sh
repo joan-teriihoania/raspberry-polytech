8ebcaaa6b878617b36e73378ea3a3805ac4f3b25
Joprocorp <joprocorp@gmail.com>
2020-12-04 20:28:30 +0100

    Adding force option to kill running instance and start another one

--- watchdog.sh
+++ watchdog.sh
@@ -1,6 +1,10 @@
+if [ "$1" = "force" ] then
+    sudo pkill -f raspberry-polytech
+    sudo python3.7 /home/jopro/raspberry-polytech/main.py
+    exit 0
+fi
 
 FILE=/home/jopro/raspberry-polytech/ressources/tmp_active_check
-
 if test -f "$FILE"; then
     rm "$FILE"
 fi
@@ -9,3 +13,5 @@ sleep 3
 if test -f "$FILE"; then
     sudo python3.7 /home/jopro/raspberry-polytech/main.py
 fi
+
+exit 0
