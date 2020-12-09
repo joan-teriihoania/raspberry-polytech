88f0d314f8933e328feb1a64eac5dc2eb83b842e
Joprocorp <joprocorp@gmail.com>
2020-12-09 20:06:37 +0100

    Fix

--- web/public/assets/js/too_many_request_checker.js
+++ web/public/assets/js/too_many_request_checker.js
@@ -15,7 +15,7 @@ var tooManyRequestsChecker = setInterval(function(){
             Swal.fire({
                 icon: "warning",
                 title: "Anti-spam",
-                text: "<b>Avertissement</b><br>Vous envoyez trop de requête au serveur. Votre adresse a été placée en liste d'avertissement, si vous en envoyez trop sur une trop longue durée, votre adresse IP sera blacklistée.",
+                html: "<b>Avertissement</b><br>Vous envoyez trop de requête au serveur. Votre adresse a été placée en liste d'avertissement, si vous en envoyez trop sur une trop longue durée, votre adresse IP sera blacklistée.",
                 footer: '<div class="alert alert-warning" role="alert">Veillez à n\'ouvrir qu\'un nombre strictement nécessaire d\'onglets.</div>',
             })  
         }
@@ -26,7 +26,7 @@ var tooManyRequestsChecker = setInterval(function(){
             Swal.fire({
                 icon: "error",
                 title: "Anti-spam",
-                text: "<b>IP blacklistée</b><br>Vous avez envoyé trop de requête au serveur et votre adresse IP a été blacklistée. Envoyez un message aux administrateurs pour que votre adresse soit réautorisée.",
+                html: "<b>IP blacklistée</b><br>Vous avez envoyé trop de requête au serveur et votre adresse IP a été blacklistée. Envoyez un message aux administrateurs pour que votre adresse soit réautorisée.",
                 footer: '<div class="alert alert-warning" role="alert">Veillez à n\'ouvrir qu\'un nombre strictement nécessaire d\'onglets.</div>',
             })  
         }
