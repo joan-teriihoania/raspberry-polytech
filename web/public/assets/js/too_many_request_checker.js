2197603fe1a6ed522fcf946687f84f528c0fdd10
Joprocorp <joprocorp@gmail.com>
2020-12-09 20:04:29 +0100

    Changing title for popup

--- web/public/assets/js/too_many_request_checker.js
+++ web/public/assets/js/too_many_request_checker.js
@@ -14,7 +14,7 @@ var tooManyRequestsChecker = setInterval(function(){
             antiSpamWarned = true
             Swal.fire({
                 icon: "warning",
-                title: "Anti-spam",
+                title: "<b>Anti-spam</b><br>Avertissement",
                 text: "Vous envoyez trop de requête au serveur. Votre adresse a été placée en liste d'avertissement, si vous en envoyez trop sur une trop longue durée, votre adresse IP sera blacklistée.",
                 footer: '<div class="alert alert-warning" role="alert">Veillez à n\'ouvrir qu\'un nombre strictement nécessaire d\'onglets.</div>',
             })  
@@ -25,7 +25,7 @@ var tooManyRequestsChecker = setInterval(function(){
             stop_tooManyRequestsChecker()
             Swal.fire({
                 icon: "error",
-                title: "Anti-spam",
+                title: "<b>Anti-spam</b><br>IP blacklistée",
                 text: "Vous avez envoyé trop de requête au serveur et votre adresse IP a été blacklistée. Envoyez un message aux administrateurs pour que votre adresse soit réautorisée.",
                 footer: '<div class="alert alert-warning" role="alert">Veillez à n\'ouvrir qu\'un nombre strictement nécessaire d\'onglets.</div>',
             })  
