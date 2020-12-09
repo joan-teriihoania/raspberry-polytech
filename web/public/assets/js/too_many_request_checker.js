722a655b843cbda4de85f49230c6a2d600ddb30a
Joprocorp <joprocorp@gmail.com>
2020-12-09 19:56:15 +0100

    Fixing user warning

--- web/public/assets/js/too_many_request_checker.js
+++ web/public/assets/js/too_many_request_checker.js
@@ -1,24 +1,23 @@
 var tooManyRequestsChecker = setInterval(function(){
-    if(XMLHttpRequest.getResponseHeader('JZ-Translation-antispam') != "WARN") {
-        return
-    }
-
-    $.ajax({
-        url: "/api/v1/ping",
-        statusCode: {
-            200: function(response, status, xhr) {
-                if(!response.is_auth){
-                    stop_tooManyRequestsChecker()
-                    Swal.fire({
-                        icon: "warning",
-                        title: "Anti-spam",
-                        text: "Vous envoyez trop de requête au serveur. Votre adresse a été placée en liste d'avertissement, si vous en envoyez trop sur une trop longue durée, votre adresse IP sera blacklistée.",
-                        footer: '<div class="alert alert-warning" role="alert">Veillez à n\'ouvrir qu\'un nombre strictement nécessaire d\'onglets.</div>',
-                    })
-                }
-            }
+    var client = new XMLHttpRequest();
+    client.open("GET", "/api/v1/ping", true);
+    client.send();
+    
+    client.onreadystatechange = function() {
+      if(this.readyState == this.HEADERS_RECEIVED) {
+        var contentType = client.getResponseHeader("JZ-Translation-antispam");
+        if (contentType == "WARN") {
+            stop_tooManyRequestsChecker()
+            Swal.fire({
+                icon: "warning",
+                title: "Anti-spam",
+                text: "Vous envoyez trop de requête au serveur. Votre adresse a été placée en liste d'avertissement, si vous en envoyez trop sur une trop longue durée, votre adresse IP sera blacklistée.",
+                footer: '<div class="alert alert-warning" role="alert">Veillez à n\'ouvrir qu\'un nombre strictement nécessaire d\'onglets.</div>',
+            })  
         }
-    });
+        client.abort();
+      }
+    }
 }, 1000)
 
 function stop_tooManyRequestsChecker(){
