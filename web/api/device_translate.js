4f53297c84657559a96ef3afb1a699723d3401b2
Joprocorp <joprocorp@gmail.com>
2020-12-10 19:56:53 +0100

    Fixing and adding connection to API

--- web/api/device_translate.js
+++ web/api/device_translate.js
@@ -9,17 +9,25 @@ module.exports = {
                 target: req.query.to_lang
             }, function(result) {
                 if(result instanceof Error){
-                    res.status(509)
+                    res.status(500)
                     res.send({error: "Unable to translate"})  
+                    return
+                }
+                var payed = true
+                if(!payed){
+                    res.status(402)
+                    res.send({error: "Your translation use has exceeded your subscription"})
                     return
                 }
 
                 res.status(200)
                 res.send({translation: result.sentences[0].trans})
+                return
             });
         } else {
             res.status(400)
             res.send({error: "Arguments missing"})
+            return
         }
     }
 }
