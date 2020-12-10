71427adc2bba7c97d2c5948f65db7ad215d221cc
Joprocorp <joprocorp@gmail.com>
2020-12-10 19:57:03 +0100

    Connecting to API

--- translater.py
+++ translater.py
@@ -1,7 +1,8 @@
 import html
-import urllib.request
-from translate import Translator
-from translate.providers import base
+import urllib
+import json
+#from translate import Translator
+#from translate.providers import base
 import core
 
 # @Desc : Translate given text from a language to another
@@ -14,24 +15,29 @@ import core
 #   UsageLimit: If the API has used the daily quota of translation assigned
 def translate(text, from_lang="french", to_lang="english"):
     device_id = 41
-    base_url = "https://raspberry-polytech.joanteriihoania.repl.co/"
+    base_url = "https://raspberry-polytech.joanteriihoania.repl.co"
     api_url = "/api/v1/device/" + str(device_id) + "/translate"
-    data = urllib.request.urlopen(base_url + api_url)
-    if(data.getCode() == 200):
-        content = data.read()
-        content = content.json()
-        return content.translation
-    return None
+    try:
+        data = urllib.request.urlopen(base_url + api_url + "?from_lang="+from_lang+"&to_lang="+to_lang+"&text="+urllib.parse.quote(text))
+        if(data.status == 200):
+            content = str(data.read().decode('UTF-8'))
+            content = json.loads(content)
+            return content['translation']
+        
+        if(data.status == 402):
+            core.terminate(1)
 
-def translate_with_Translator(text, from_lang="french", to_lang="english"):
-    translator = Translator(from_lang=from_lang, to_lang=to_lang)
-    translation = translator.translate(text)
+        return None
+    except:
+        return None
+
+# def translate_with_Translator(text, from_lang="french", to_lang="english"):
+#     translator = Translator(from_lang=from_lang, to_lang=to_lang)
+#     translation = translator.translate(text)
     
-    if("HTTPS://MYMEMORY.TRANSLATED.NET/DOC/USAGELIMITS.PHP" in translation):
-        core.terminate(1)
+#     if("HTTPS://MYMEMORY.TRANSLATED.NET/DOC/USAGELIMITS.PHP" in translation):
+#         core.terminate(1)
 
-    if(isinstance(translation, str)):
-        translation = html.unescape(translation)
-    return translation
-
-print(translate("salut tout le monde", "fr", "en"))
+#     if(isinstance(translation, str)):
+#         translation = html.unescape(translation)
+#     return translation
