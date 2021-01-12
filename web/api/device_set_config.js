4357644e485469ea772c8412ba56918a918438ca
Joprocorp <joprocorp@gmail.com>
2021-01-12 18:42:44 +0100

    Fixing device_set_config error

--- web/api/device_set_config.js
+++ web/api/device_set_config.js
@@ -4,7 +4,7 @@ var {database} = require('../server');
 
 module.exports = {
     exec: function(req, res){
-        if(req.body.from_lang && req.body_to_lang){
+        if(req.body.from_lang && req.body.to_lang){
             db.select(database, 'SELECT * FROM devices WHERE user_id = '+res.user.user_id+' AND device_id = '+req.params.device_id, function(devices){
                 if(devices && devices.length > 0){
                     db.run(database, 'UPDATE devices SET from_lang = "'+req.body.from_lang+'", to_lang = "'+req.body.to_lang+'" WHERE device_id = ' + devices[0].device_id).then(() => {
